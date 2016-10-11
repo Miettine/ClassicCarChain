c57c7cf71fef39afa2dd9fdd71474c3f2cab9684
Miettine <lauri.miettinen90@gmail.com>
2016-10-11 13:00:49 +0300

    Uudistettu nykyisten tietojen valossa.

--- SmartContracts/Beggar.sol
+++ SmartContracts/Beggar.sol
@@ -12,23 +12,21 @@ to ask people to send them money.
 Useful example: http://solidity.readthedocs.io/en/develop/solidity-by-example.html
 */
 
-    mapping (address => uint) public balances; 
-	//Do I need something like this?
-	//I think that this is needed only if I'm making a token.
-	//My intention is to make a contract that can send Ether, not tokens.
 
      address public beggarAddress; 
-    //I'm not sure if this is correct. From what I understood, 
-    //this field is stored to this contract object.
+     
     address public beggedPerson;
     
     uint public beggedSum;
 
     mapping(address => uint) pendingReturns;
+	//Do I need something like this?
+	//I think that this is needed only if I'm making a token.
+	//My intention is to make a contract that can send Ether, not tokens.
+
 	
     // Events allow light clients to react on
     // changes efficiently
-    //Not sure if I actually need these.
     event BegForMoneyEvent(address beggar, address giver, uint beggedSum);
     event BegAcceptedEvent(address beggar, address giver, uint beggedSum);
 
@@ -36,14 +34,14 @@ Useful example: http://solidity.readthedocs.io/en/develop/solidity-by-example.ht
     // This is the constructor whose code is
     // run only when the contract is created.
     function Beggar(address _beggarAddress) {
-        beggarAddress = _beggarAddress;
+        beggarAddress = msg.sender;
     }
 
     function Beg( address _beggedPerson, uint _amount) {
         beggedPerson = _beggedPerson;
         beggedSum = _amount;
         
-        BegForMoneyEvent( beggarAddress, _beggedPerson, _amount);
+        BegForMoneyEvent(beggarAddress, _beggedPerson, _amount);
     }
 
     function Accept() {
@@ -54,12 +52,12 @@ Useful example: http://solidity.readthedocs.io/en/develop/solidity-by-example.ht
         //Anyone can see this function, but only if the beggar has asked
         //that address for money, can the money be sent.
         
-        if (balances[msg.sender] < beggedSum) return;
+        if (msg.value < beggedSum) return;
         
-        balances[msg.sender] -= beggedSum;
-        balances[beggarAddress] += beggedSum;
+        if ( !beggedPerson.send(beggedSum)){
+            
+             BegAcceptedEvent( beggarAddress, msg.sender, beggedSum);
+        }
         
-        
-        BegAcceptedEvent( beggarAddress, msg.sender, beggedSum);
     }
 }
