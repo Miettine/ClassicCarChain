df887d732b5d59ee9c61c56a75376cdaf33b0eb1
Miettine <lauri.miettinen90@gmail.com>
2016-10-13 18:27:24 +0300

    Uusi versio

--- SmartContracts/Philanthropist.sol
+++ SmartContracts/Philanthropist.sol
@@ -39,30 +39,43 @@ Useful example: http://solidity.readthedocs.io/en/develop/solidity-by-example.ht
         BegForMoneyEvent(msg.sender, _amountInEther);
     }
 
-    function Accept(address _beggar) returns (bool) {
+    function Accept(address _beggarAddress) returns (bool) {
        
         //1: Only the Philantropist is allowed to accept begs. No-one else. 
-        if (msg.sender != philantropistAddress)   throw; 
+        if (msg.sender != philantropistAddress){
+            throw; //Reminder to self: throw consumes all provided gas!
+            //http://solidity.readthedocs.io/en/develop/solidity-by-example.html
+            //See Voting-example
+        }
         
         //2: Check if the beggar's address is valid. 
         //If the address supplied by the philantropist 
         //is not found on the list of beggars, cancel this function call.
         
+        //https://www.reddit.com/r/ethereum/comments/3j64hz/how_can_you_figure_out_if_a_certain_key_exists_in/
+        //if (_beggarAddress )
         
+        //Aaaargh! I don't understand how to do this :(
         
         //3: Check if the philantropist actually has enough money.
         
-         uint amount = begs[_beggar];
+         uint amount = begs[_beggarAddress];
          
          if (philantropistAddress.balance < amount) {
              throw;
          }
         
         //4: Send the money to the beggar
-        
-        
+
+        if (!msg.sender.send(amount))
+            throw;
+        //Not entirely sure what the if-clause is needed for.
+        //I think it checks if the transaction was successful and throws if it wasn't.
         
         //5: remove the beggar from the list of begs.
+        
+        begs[_beggarAddress]=0; //Zero this, just in case I misunderstood how the delete keyword works.
+        delete begs[_beggarAddress];
 
     }
 }
