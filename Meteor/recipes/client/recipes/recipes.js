8576dd7be1c097285d601e6dc189ee131b3d86bb
Miettine <lauri.miettinen90@gmail.com>
2016-11-06 18:54:40 +0200

    Reseptit näkyy. Nyt tuli paljon oppia.

--- Meteor/recipes/client/recipes/recipes.js
+++ Meteor/recipes/client/recipes/recipes.js
@@ -1,1 +1,11 @@
 Meteor.subscribe('recipes'); //Uhh? What does ths mean? What does it subscribe to?
+
+Template.Recipes.helpers({
+	//I messed up here at first, because I wrote "recipe", instaed of "recipes"
+	recipes: ()=>{
+		return Recipes.find({
+//Gets content of the recipes schema (or whatever its supposed to be called)
+// from the database 
+		});
+	}
+});
