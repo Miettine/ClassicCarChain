279b2b9d5d235914a90cb2aaef56bfa15daa7e66
Miettine <lauri.miettinen90@gmail.com>
2016-11-21 16:18:33 +0200

    Koitan tehdä funktiokutsua sopimukseen.

--- Meteor/myDapp/client/lib/init.js
+++ Meteor/myDapp/client/lib/init.js
@@ -1,22 +1,17 @@
 if(typeof web3 === 'undefined'){
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));}
 
-
+//As a practice, I attempt to call a function in a simple smart contract I made. The goal is to get a integer number from a contract named "storage"
 
 
 // contract abi
-var abi = [{"constant":false,"inputs":[{"name":"_amountInEther","type":"uint256"}],"name":"Beg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"philantropistAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_beggarAddress","type":"address"}],"name":"Accept","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"BegForMoneyEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"MoneyDonatedEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"BegRejectedEvent","type":"event"}];
 
-// creation of contract object
-var MyContract = web3.eth.contract(abi);
-var myContractInstance = MyContract.at('0x2e1fBA1e5d279f382E69aBb6EFA4CE25c245C485');
+//Storage-contract address: 0x9E79d08e2ce653E9D1770CA101423f447c5f2d13
 
 
 /*
 //
 
-// contract abi
-var abi = [{"constant":false,"inputs":[{"name":"_amountInEther","type":"uint256"}],"name":"Beg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"philantropistAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_beggarAddress","type":"address"}],"name":"Accept","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"BegForMoneyEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"MoneyDonatedEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"beggar","type":"address"},{"indexed":false,"name":"beggedSum","type":"uint256"}],"name":"BegRejectedEvent","type":"event"}];
 
 // creation of contract object
 var MyContract = web3.eth.contract(abi);
