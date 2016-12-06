8abb12df046e73347f2ab1dd87ea97b396eade8e
Miettine <lauri.miettinen90@gmail.com>
2016-12-06 14:15:57 +0200

    Älä varasta

--- ContractDev/test/philanthropist.js
+++ ContractDev/test/philanthropist.js
@@ -10,13 +10,40 @@ contract('Philanthropist', function(accounts) {
 		});
 
 	});
+	
+	it("should not be possible to steal philanthropist rights", function() {
+		
+		var philContract = Philanthropist.deployed();
+
+		// Get initial balances of first and second account.
+		var phil;
+		var thief = accounts[3];
+		var observer = accounts[4];
+
+		return philContract.GetPhilanthropistAddress.call(observer).then(function(address) {
+			
+			phil = address.valueOf();
+			
+			return  philContract.GivePhilanthropistRights(thief, {from: thief}).then(function() {
+				
+				return philContract.GetPhilanthropistAddress.call(observer).then(function(address) {
+				
+					assert.equal(address.valueOf(), phil, "The philanthropist rights were changed after the theft attempt"); 
+
+					assert.notEqual(address.valueOf(), thief, "The thief became the philanthropist after the theft attempt");
+					//In this case, message is printed if address is the same as observer
+					
+				});
+			});
+		});
+	});
   
   
 	it("should pass philantropist rights to the appointed address", function() {
 		var philContract = Philanthropist.deployed();
 
 		// Get initial balances of first and second account.
-		var phil = accounts[0];
+		var phil;
 		var newPhil = accounts[1];
 		var observer = accounts[2];
 
@@ -24,29 +51,26 @@ contract('Philanthropist', function(accounts) {
 		//2: Make the current phil give rights to another address
 		//3: See if the new phil is the same as the appointed phil.
 	
-		return  philContract.GivePhilanthropistRights(newPhil, {from: phil}).then(function() {
-			
-			
 		return philContract.GetPhilanthropistAddress.call(observer).then(function(address) {
-		
-		assert.equal(address.valueOf(), newPhil, "The addresses are not the same"); 
-		//Reminder: The message is printed if this test fails
-		//In this case, if address is not the same as newPhil
-		assert.notEqual(address.valueOf(), observer, "The observer somehow became the philanthropist");
-		//In this case, message is printed if address is the same as observer
-		assert.notEqual(address.valueOf(), phil, "The former philanthopist still has the rights");
-		//In this case, message is printed if oldPhil is the same as address
-		
+			
+			phil = address.valueOf();
+			
+			return philContract.GivePhilanthropistRights(newPhil, {from: phil}).then(function() {
+				
+				return philContract.GetPhilanthropistAddress.call(observer).then(function(address) {
+			
+					assert.equal(address.valueOf(), newPhil, "The addresses are not the same"); 
+					//Reminder: The message is printed if this test fails
+					//In this case, if address is not the same as newPhil
+					assert.notEqual(address.valueOf(), observer, "The observer somehow became the philanthropist");
+					//In this case, message is printed if address is the same as observer
+					assert.notEqual(address.valueOf(), phil, "The former philanthopist still has the rights");
+					//In this case, message is printed if oldPhil is the same as address
+			
+				});
+			});
 		});
-
-  
-  	});
 	});
-	/*
-	it("should not be possible to steal philanthropist rights", function() {
-		//Some person attempts to take the phil rights
-	});
-	*/
+	
+	
 });
-
-
