8e0047bf96b3c46acdd939ec2f8f6db61228f883
Miettine <lauri.miettinen90@gmail.com>
2016-12-06 17:02:21 +0200

    Kesti kauan, mutta keksin miten tilien balansseja saa testattua.

--- ContractDev/test/philanthropist.js
+++ ContractDev/test/philanthropist.js
@@ -37,8 +37,48 @@ contract('Philanthropist', function(accounts) {
 			});
 		});
 	});
-  
-  
+	
+	it("should be able to send money to a beggar", function() {
+		var philContract = Philanthropist.deployed();
+
+		// Get initial balances of first and second account.
+		var phil = accounts[0];
+		var beggar = accounts[1];
+		var observer = accounts[2];
+
+		var beggedAmount=1;
+		
+		//1: Get who the current phil is.
+		//2: Have the beggar make a beg
+		//3: Have the phil accept it
+		//4: check if the balances are correct
+		//5: check if the beggar's address was removed from the list of begs.
+	
+		var balance = web3.eth.getBalance(accounts[0]).toNumber();
+		console.log(balance);
+		//var phil_starting_balance = web3.fromWei(web3.eth.getBalance(phil));
+		//return web3.eth.getBalance(phil).then(function(balance) {
+			/*
+			var phil_starting_balance = balance.valueOf();
+			
+			return philContract.Beg(beggedAmount, {from: beggar}).then(function() {
+			
+
+				return philContract.Accept({from: phil}).then(function() {
+			
+				
+					//assert.equal(phil_ending_balance, phil_starting_balance - beggedAmount, "Amount wasn't correctly taken from the philanthropist");
+					//assert.equal(beggar_ending_balance, beggar_starting_balance + beggedAmount, "Amount wasn't correctly sent to the beggar");
+				});
+		
+			});*/
+		//});
+	});
+	/*
+	function getCurrentPhil(address _callAddress){
+		
+	}
+  */
 	it("should pass philantropist rights to the appointed address", function() {
 		var philContract = Philanthropist.deployed();
 
@@ -71,6 +111,7 @@ contract('Philanthropist', function(accounts) {
 			});
 		});
 	});
+	
 	
 	
 });
