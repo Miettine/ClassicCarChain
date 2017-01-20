00043b1c610027aa68813e55a123f50d1b435ad3
Miettine <lauri.miettinen90@gmail.com>
2017-01-20 14:13:57 +0200

    ...

--- Meteor/ClassicCarChain/client/ethereum/04-eth-events.js
+++ Meteor/ClassicCarChain/client/ethereum/04-eth-events.js
@@ -1,25 +1,35 @@
 Ethereum = (function (Events) {
 
-	var allEventsArray=[];
+	
 
 	var events = Ethereum.contractInstance.allEvents({fromBlock: 0, toBlock: 'latest'});
 
-	events.watch(function(error, result){
+	var arrayEVehicleInformationUpdated = [];
+	var eVehicleInformationUpdated = Ethereum.contractInstance.EVehicleInformationUpdated({fromBlock: 0, toBlock: 'latest'});
+
+	eVehicleInformationUpdated.watch(function(error, result){
 		if (err) {
-			console.log("events, error: "+err);
+			console.log("eVehicleInformationUpdated, error: "+err);
 
 			return;
 		}
 		
-		allEventsArray.push(result.args);
+		arrayEVehicleInformationUpdated.push(result.args);
 
-		Session.set('allEventsArray', allEventsArray);
-		console.log("allEventsArray: "+result.args);
+		Session.set('eVehicleInformationUpdated', arrayEVehicleInformationUpdated);
+		console.log(arrayEVehicleInformationUpdated);
 		// append details of result.args to UI
-	});
-
+   });
+/*
+EVehicleInformationUpdated
+EHighlightRequestMade
+EHighlightSavedToChain
+EHighlightDeleted
+EVehicleOwnershipPassed
+EErrorOccurred
+*/
 	Events.allEvents = function () {
-		return allEventsArray;
+		return arrayEVehicleInformationUpdated;
 	};
 
 	return Events;
