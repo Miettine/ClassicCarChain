9a5d1a1c6ead4ee2f62b32abd4eee3384076dc1c
Miettine <lauri.miettinen90@gmail.com>
2017-02-21 12:58:22 +0200

    Valmista tuli

--- ContractDev/contracts/ClassicCarChain.sol
+++ ContractDev/contracts/ClassicCarChain.sol
@@ -11,43 +11,55 @@ contract ClassicCarChain {
 	uint public vehicleManufacturingYear;
 	
 	/// This index is used as an identifier of Highlights. It is incremented whenever a new highlight request is made.
-	uint public highlightIndex=0;
+	//You'll know that a highlight doesn't exist if a zero-value is returned.
+	uint public highlightIndex=1;
 
 	/////////////////////////////////////////
 
-//Due to design considerations, it is important that this mapping and array are not made public. Only use other functions in this contract to modify them.
-
-	//mapping(uint => CCClib.Highlight) private highlights;
-	CCClib.Highlight[] private highlights;
+	mapping(uint => CCClib.Highlight) private highlights;
+	uint[] private highlightsArray;
 	
 	/////////////////////////////////////////
 	
 	function GetHighlightsArrayLength() public returns (uint){
-		return highlights.length;
+		return highlightsArray.length;
 	}
 
-	function AddNewToHighlights(CCClib.Highlight _h) private returns(bool) {
-		if (HighlightExists(_h.id)){
-			return false;
+	function AddNewToHighlights(CCClib.Highlight _h) private {
+
+	    highlights[_h.id]=_h;
+        highlightsArray.push(_h.id);
+        
+    	highlightIndex += 1;
+	}
+
+	function RemoveFromHighlights(uint _id) private {
+
+	    delete highlights[_id];
+
+	    for (uint i = 0; i< highlightsArray.length ; i++){
+			if (highlightsArray[i]==_id) {
+				highlightsArray= CCClib.RemoveFromArray(i, highlightsArray);
+			}
 		}
-
-
-		highlights.push(_h);
-
-		return true;
 	}
-	
-	function HighlightExists (uint _id) public returns(bool){
+
+ 
+
+	/*
+	modifier HighlightExists (uint _id) public {
+		bool exists= false;
 		for (uint i = 0; i< highlights.length ; i++){
-			if (highlights[i].id==_id) {
-				return true;
+			if (highlights[_id].id==_id) {
+				exists=true;
 			}
 		}
 
 		return false;
 	}
+	*/
 
-	function GetHighlight(uint _index) public returns (
+	function GetHighlight(uint _id) public returns (
 		address _maker, 
 		uint _requestCreationDateTime, 	
 		uint _reward, 
@@ -57,7 +69,7 @@ contract ClassicCarChain {
 		uint _additionToChainDateTime
 		) {
 		
-		CCClib.Highlight h = highlights[_index];
+		CCClib.Highlight h = highlights[_id];
 		
 		_maker = h.maker;
 		_requestCreationDateTime = h.requestCreationDateTime;
@@ -82,7 +94,114 @@ contract ClassicCarChain {
 	}
 */
 
-////O//||=================>
+
+	modifier OnlyByOwner()
+	{
+		if (msg.sender == vehicleOwner) {
+		
+			_;
+		}
+	}
+	
+		modifier NotByOwner()
+	{
+		if (msg.sender != vehicleOwner) {
+		
+			_;
+		}
+	}
+	
+	
+	function AddHighlightAsOwner (string _message) OnlyByOwner() public {
+
+		CCClib.Highlight memory h = CCClib.NewHighlight(highlightIndex, 0, _message);
+
+		AddNewToHighlights(h);
+
+		EmitEvent_HighlightSavedToChain(h);
+
+	}
+	
+	function MakeHighlightRequest(uint _reward, string _message) NotByOwner() public {
+	    
+    	CCClib.Highlight memory h = CCClib.NewHighlightRequest (highlightIndex, _reward, _message);
+	
+		AddNewToHighlights(h);
+		
+		EmitEvent_HighlightRequestMade(h);
+
+	
+	}
+	
+	function DeleteExistingHighlight(uint _id, string _reasonForDeletion) OnlyByOwner() public returns (bool) {
+		//Deleting a key in a mapping replaces the struct of that 
+		//key with a struct posessing default-values.
+		
+		EmitEvent_HighlightDeleted(highlights[_id], _reasonForDeletion);
+
+		RemoveFromHighlights(_id);
+	}
+	
+	function RejectHighlightRequest(uint _id) OnlyByOwner()  {
+
+  		EmitEvent_HighlightRequestRejected(highlights[_id]);
+		
+		RemoveFromHighlights(_id);
+	}
+
+	function AcceptHighlightRequest(uint _id) OnlyByOwner() returns (bool)  {
+		//TODO: Find out if this function needs to have the payable-keyword.
+		//Is there some security restriction, that a contract cannot send funds if
+		// the message sender doesn't send them?
+		
+		// Check if the owner actually has enough money.
+	
+	    CCClib.Highlight handledRequest = highlights[_id];
+	
+		if (vehicleOwner.balance < handledRequest.reward) {
+			return false;
+		}
+		
+		// Send the money to the maker
+
+		if ( handledRequest.maker.send(handledRequest.reward)) {
+
+			highlights[_id].approvedToChain=true;
+		    highlights[_id].additionToChainDateTime=now;
+			
+			EmitEvent_HighlightSavedToChain(highlights[_id]);
+            
+			return true;
+		}
+		
+		return false;
+	}
+	
+	function GiveVehicleOwnership(address _newOwner) OnlyByOwner()  {
+		if (_newOwner!=vehicleOwner){
+			address oldOwner = vehicleOwner;
+			
+			EVehicleOwnershipPassed( oldOwner,  _newOwner, now);
+			// The now-keyword returns the current block timestamp, as soon as this transaction finds its way into a mined block.
+			// I remember hearing that in the real Ethreum network, blocks are mined each 10 minutes. The timestamp is quite accurate.
+			vehicleOwner = _newOwner;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	////O//||=================>
+
+
+
+
+/// Events
+
 
 	event EHighlightRequestMade(
 		uint highlightId,
@@ -155,101 +274,14 @@ contract ClassicCarChain {
 		vehicleModel = _model;
 		vehicleManufacturingYear = _year;
 	}
-	
-	modifier OnlyByOwner()
-	{
-		if (msg.sender == vehicleOwner) {
-		
-			_;
-		}
-	}
-	
-		modifier NotByOwner()
-	{
-		if (msg.sender != vehicleOwner) {
-		
-			_;
-		}
-	}
-	
-	function AddHighlightAsOwner (string _message) OnlyByOwner() public {
-
-		CCClib.Highlight memory h = CCClib.NewHighlight(highlightIndex, 0, _message);
-
-		AddNewToHighlights(h);
-
-		EmitEvent_HighlightSavedToChain(h);
-
-		highlightIndex += 1;
-	}
-	
-	function MakeHighlightRequest(uint _reward, string _message) NotByOwner() public {
-	    
-    	CCClib.Highlight memory h = CCClib.NewHighlightRequest (highlightIndex, _reward, _message);
-	
-		highlights[highlightIndex] = h;
-		
-		EmitEvent_HighlightRequestMade(h);
-
-		highlightIndex += 1;
-	}
-	
-	function DeleteExistingHighlight(uint _id, string _reasonForDeletion) OnlyByOwner() public {
-		//Deleting a key in a mapping replaces the struct of that 
-		//key with a struct posessing default-values.
-		
-		EmitEvent_HighlightDeleted(highlights[_id], _reasonForDeletion);
-		
-		delete highlights[_id];
-	}
-	
-	function RejectHighlightRequest(uint _id) OnlyByOwner()  {
-
-  		EmitEvent_HighlightRequestRejected(highlights[_id]);
-		
-		delete highlights[_id];
-	}
-
-	function AcceptHighlightRequest(uint _id) OnlyByOwner() returns (bool)  {
-		//TODO: Find out if this function needs to have the payable-keyword.
-		//Is there some security restriction, that a contract cannot send funds if
-		// the message sender doesn't send them?
-		
-		// Check if the owner actually has enough money.
-	
-	    CCClib.Highlight handledRequest = highlights[_id];
-	
-		if (vehicleOwner.balance < handledRequest.reward) {
-			return false;
-		}
-		
-		// Send the money to the maker
-
-		if ( handledRequest.maker.send(handledRequest.reward)) {
-
-			//PromoteHighlightRequest(CCClib.NewHighlight(handledRequest));
-			
-			EmitEvent_HighlightSavedToChain(highlights[_id]);
-			
-            //highlight index is not incremented here.
-            
-			return true;
-		}
-		
-		return false;
-	}
-	
-	function GiveVehicleOwnership(address _newOwner) OnlyByOwner()  {
-		if (_newOwner!=vehicleOwner){
-			address oldOwner = vehicleOwner;
-			
-			EVehicleOwnershipPassed( oldOwner,  _newOwner, now);
-			// The now-keyword returns the current block timestamp, as soon as this transaction finds its way into a mined block.
-			// I remember hearing that in the real Ethreum network, blocks are mined each 10 minutes. The timestamp is quite accurate.
-			vehicleOwner = _newOwner;
-		}
-	}
 }
+
+
+
+
+
+
+
 
 
 
@@ -295,31 +327,28 @@ library CCClib {
 			additionToChainDateTime:now
 		});
 	}
+
+	function RemoveFromArray(uint _index, uint[] storage _array) internal returns(uint[]) {
+        if (_index >= _array.length) return;
+
+        for (uint i = _index; i<_array.length-1; i++){
+            _array[i] = _array[i+1];
+        }
+        delete _array[_array.length-1];
+        _array.length--;
+        return _array;
+    }
 	
 	function bleh(uint[] maints, bool[] done) {
 	    if (maints.length != done.length) {
 	    	return;
 	    }
 	}
-		
-	function NewHighlight (uint _id, uint _reward,string _message, string[] maints) internal returns ( Highlight){
-			
-		return Highlight({
-			id: _id,
-			maker: msg.sender,
-			requestCreationDateTime: now,
-			reward: _reward,
-			message: _message,
-			approvedToChain: true,
-			madeByOwner: true,
-			additionToChainDateTime:now
-		});
-	}
-	
+/*
 	function PromoteHighlight (Highlight _h) internal returns ( Highlight){
 		
 		_h.approvedToChain=true;
 		_h.additionToChainDateTime=now;
 		return _h;
-	}
+	}*/
 }
