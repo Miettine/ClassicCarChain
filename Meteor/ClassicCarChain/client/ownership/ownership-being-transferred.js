e95940d98d1007d0b96efb1a13255e52a6fadeae
Miettine <lauri.miettinen90@gmail.com>
2017-03-18 19:51:41 +0200

    no message

--- Meteor/ClassicCarChain/client/ownership/ownership-being-transferred.js
+++ Meteor/ClassicCarChain/client/ownership/ownership-being-transferred.js
@@ -4,3 +4,16 @@ Template.Offers.helpers( {
 		return Ethereum.Offers.ownershipBeingTransferred();
 	});
 });
+
+Template.Offer.events( {
+
+	'click .cancel-button': function(){
+    	console.log("Clicked cancel offer");
+    	Ethereum.Offers.cancelOwnershipChange();
+	},
+
+	'click .accept-button': function(){
+    	console.log("Clicked gain ownership");
+    	Ethereum.Offers.acceptOwnershipChange();
+	}
+});
